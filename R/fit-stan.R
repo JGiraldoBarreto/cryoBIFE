a9b8107e27209efe35ac3148c0e0e953a5bb4616
Bob Carpenter <bcarpenter@flatironinstitute.org>
2021-10-19 16:10:28 -0400

    move stan models; MAP in R script

--- R/fit-stan.R
+++ R/fit-stan.R
@@ -9,19 +9,29 @@ library("ggplot2")
 options(width=150)
 options(cmdstanr_max_rows=1e3)
 
+printf <- function(msg, ...) cat(sprintf(msg, ...), "\n")
+
 Pmat <- read.csv("../data/Pmat.tsv", sep="\t")
 N <- dim(Pmat)[1]
 M <- dim(Pmat)[2]
 data <- list(M = M, N = N, Pmat = Pmat)
 
-mod <- cmdstan_model("../stan/cryo-bife.stan")
+mod <- cmdstan_model("../cryoBIFE/stan/cryo-bife.stan")
 fit <- mod$sample(data = data, chains = 4, parallel_chains = 4)
 
-mod_marginal <- cmdstan_model("../stan/cryo-bife-marginal.stan")
+mod_marginal <- cmdstan_model("../cryoBIFE/stan/cryo-bife-marginal.stan")
 fit_marginal <- mod_marginal$sample(data = data, chains = 4, parallel_chains = 4)
+map_marginal <- mod_marginal$optimize(data = data);
 
+
+printf("POSTERIOR (FULL MODEL)")
 print(fit, c("rho", "lambda"))
+
+printf("POSTERIOR (MARGINALIZED MODEL)")
 print(fit_marginal, c("rho"))
+
+printf("MAP ESTIMATE (MARGINALED MODEL)")
+print(map_marginal, c("rho"))
 
 ### plot posterior simplex estimate
 rho_hat <- fit$summary("rho", mean)$mean
